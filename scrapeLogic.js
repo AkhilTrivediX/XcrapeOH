82f44f09988a68b733124340ab924de33feca1ef
DevilsKingX <118957648+AkhilTrivediX@users.noreply.github.com>
2025-02-06 12:12:18 +0530

    Insta scrape

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,50 +1,52 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
+
+const browserPromise = puppeteer.launch({
+  args: [
+    "--disable-setuid-sandbox",
+    "--no-sandbox",
+    "--single-process",
+    "--no-zygote",
+  ],
+  executablePath:
+    process.env.NODE_ENV === "production"
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+});
 const scrapeLogic = async (res) => {
-  const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
+
+  const ret = {
+    time: {
+        browserInit: 0,
+        pageInit: 0,
+        pageNav: 0,
+        pageTitle: 0,
+        total: 0
+    },
+    result: ''
+}
+let time = Date.now();
+let totalTime = time;
+  const browser = await browserPromise;
+
+  ret.time.browserInit = Date.now()-time;
+    time = Date.now();
   try {
     const page = await browser.newPage();
-
-    await page.goto("https://developer.chrome.com/");
-
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    ret.time.pageInit = Date.now()-time;
+    time = Date.now();
+    await page.goto('https://instagram.com/cristiano');
+    ret.time.pageNav = Date.now()-time;
+    time = Date.now();
+    const title = await page.title();
+    ret.time.pageTitle = Date.now()-time;
+    ret.time.total = Date.now()-totalTime;
+    ret.result = title;
+    res.send(ret)
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
   }
 };
 
